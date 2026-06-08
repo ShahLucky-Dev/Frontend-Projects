2ede69c6931c3e623f2b676ee3f0fc0b8c0a61cd
ShahLucky444 <shahlucky755@gmail.com>
2026-06-08 17:20:41 +0530

    add  js logic for dropdown flags

--- CC.js
+++ CC.js
@@ -1,2 +1,34 @@
 const url =
   "https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies";
+
+const dropdowns = document.querySelectorAll(".dropdown select");
+
+// dropdown me sare country lane ke liye
+for (let select of dropdowns) {
+  for (let currCode in countryList) {
+    let newOp = document.createElement("option");
+
+    newOp.innerText = currCode;
+    newOp.value = currCode;
+
+    select.append(newOp);
+
+    // flag change
+
+    select.addEventListener("change", (evt) => {
+      Flag(evt.target);
+    });
+  }
+}
+
+// flag changing logic
+const Flag = (element) => {
+  let currCode = element.value;
+  let countryCode = countryList[currCode];
+
+  let newsrc = `https://flagsapi.com/${countryCode}/flat/64.png`;
+
+  let img = element.parentElement.querySelector("img");
+
+  img.src = newsrc;
+};
