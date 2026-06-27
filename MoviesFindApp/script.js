5c826b0936044f71baeaf4619294bfd9eb12d4d6
ShahLucky444 <shahlucky755@gmail.com>
2026-06-27 20:58:29 +0530

    complete movie search app & add new features

--- MoviesFindApp/script.js
+++ MoviesFindApp/script.js
@@ -2,16 +2,42 @@ let inp = document.getElementById("inp");
 let btn = document.getElementById("btn");
 let result = document.getElementById("result");
 
-btn.addEventListener("click", async () => {
+async function searchMovie() {
+  if (inp.value === "") {
+    alert("Enter valid movie name");
+    return;
+  }
+  result.innerText = "Loading....";
   let response = await fetch(
     `https://www.omdbapi.com/?apikey=fac55369&t=${inp.value}`,
   );
   let data = await response.json();
-  result.innerHTML = `
+  if (data.Response === "False") {
+    result.innerHTML = `<h2>Movie not found</h2>`;
+    return;
+  }
+  // console.log(data);
+  result.innerHTML = `<div class="movie-card">
+  <img id="poster" src="${data.Poster}" width="200"></img>
+   <div class="detail">
   <h2>${data.Title}</h2>
+  
+ 
   <p>${data.Year}</p>
-  <img src "${data.Poster}" width="200"></img>
-  <p>${data.imbdRating}</p>
+  <br/>
+  <p>${data.Genre}</p>
+  <br/>
+  <p>${data.imdbRating}</p>
+  <br/>
   <p>${data.Plot}</p>
+  </div>
+  </div>
   `;
+}
+
+btn.addEventListener("click", searchMovie);
+inp.addEventListener("keydown", (e) => {
+  if (e.key === "Enter") {
+    searchMovie();
+  }
 });
